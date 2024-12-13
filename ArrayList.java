2ea2b37f46e0d9376a0465ac869aac3639fba0a9
mounikapujari <mounikapujari2005@gmail.com>
2024-12-13 15:57:37 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ArrayList{
+    public static void main(String[] args) {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(345);
+        a.add(456);
+        a.add(678);
+        System.out.println(a);
+    }
+}
